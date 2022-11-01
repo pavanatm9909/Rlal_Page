bb4dffafc4f8b5364b0c35d7b502f9f6f867028f
pavanatm9909 <S545458@NWMISSOURI.EDU>
2022-11-01 14:56:57 -0500

    contact details done

--- src/app/research/research.component.ts
+++ src/app/research/research.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./research.component.css']
 })
 export class ResearchComponent implements OnInit {
-
+  isValid: boolean | undefined;
   constructor() { }
 
   ngOnInit(): void {
+    this.isValid = true
+  }
+
+  ChangeToDetails() {
+    if (this.isValid) {
+      this.isValid = false
+    }
+    else {
+      this.isValid = true
+    }
   }
 
 }
